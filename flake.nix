b61f377f883b524f40610ea0a5a183b907b713c5
Ash Arthur <ashleyarthur98@gmail.com>
2026-05-13 03:01:20 -0700

    astro revamp

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Ashley Arthur's blog - Astro static site";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nodejs
+            pkgs.pnpm
+          ];
+        };
+      }
+    );
+}
